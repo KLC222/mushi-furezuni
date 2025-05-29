d685e60d1b6d1998fdd0b8dc1447270755d5ca7e
KLC222 <91913554+KLC222@users.noreply.github.com>
2025-05-29 17:32:21 +0100

    fix return page

--- mushi-furezuni/src/CheckoutReturn.tsx
+++ mushi-furezuni/src/CheckoutReturn.tsx
@@ -35,8 +35,11 @@ const CheckoutReturn = () => {
           return response.data
           }
     })
-    setStatus(data.status);
-    setCustomerEmail(data.customer_email);
+
+    console.log('session data', data)
+    // setStatus(data.status);
+    // setCustomerEmail(data.customer_email);
+
     //   fetch(`/session-status?session_id=${sessionId}`)
     //     .then((res) => res.json())
     //     .then((data) => {
@@ -45,17 +48,17 @@ const CheckoutReturn = () => {
     //     });
     
   
-    if (status === 'open') {
+    if (data.status === 'open') {
       return (
         <Navigate to="/checkout" />
       )
     }
   
-    if (status === 'complete') {
+    if (data.status === 'complete') {
       return (
         <section id="success">
           <p>
-            We appreciate your business! A confirmation email will be sent to {customerEmail}.
+            We appreciate your business! A confirmation email will be sent to {data.customer_email}.
   
             If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
           </p>
