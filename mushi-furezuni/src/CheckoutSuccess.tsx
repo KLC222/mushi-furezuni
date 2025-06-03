128dc174d615afc04e96da864d67405ce3df8c83
KLC222 <91913554+KLC222@users.noreply.github.com>
2025-06-03 18:35:47 +0100

    checkoutsuccess

--- mushi-furezuni/src/CheckoutSuccess.tsx
+++ mushi-furezuni/src/CheckoutSuccess.tsx
@@ -11,13 +11,13 @@ export default function CheckoutSuccess(props: Props){
     return (
      
             <Layout>
-          
-                <Row>
+                
+                <Row className="pt-5">
                     <Col><h2>ご購入ありがとうございます</h2></Col>
                 </Row>
                 <Row>
-                    <Col>
-                        <p>お客様のメール宛に購入完了のメールを送信させていただきました。</p>
+                    <Col className="pt-5">
+                        <p>ご注文を承りました。お客様のメール宛に購入完了のメールを送信させていただきました。</p>
                         <p>商品の到着まで、今しばらくお待ちください。</p>
                         <p>ご質問がありましたら<a href="mailto:orders@example.com">orders@example.com</a>までお気軽にお問い合わせください。</p>
                     </Col>
